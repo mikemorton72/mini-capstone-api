47453be6d5187478c4ad2837787d64be708fa96d
mike <mmorton7295@gmail.com>
2021-06-17 19:42:17 -0500

    routes and controllers recreated to match convention, route and action added for create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,24 @@
 class ProductsController < ApplicationController
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
 
+  def show
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    render json: product.as_json
+  end
+  
+  def create
+    product = Product.new(
+      name: params[:name], 
+      price: params[:price], 
+      image_url: params[:image_url], 
+      description: params[:description]
+    )
+    #this needs params from user.
+    product.save
+
+  end
 end
