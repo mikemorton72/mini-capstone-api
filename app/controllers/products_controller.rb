41469f8cd68b3b0f4228a4f77704bf931338ed52
mike <mmorton7295@gmail.com>
2021-06-17 21:12:59 -0500

    adds update route and action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,18 @@ class ProductsController < ApplicationController
     product.save
     render json: product.as_json
   end
+
+  def update
+    # render json: {output: "hello"}
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.update(
+    name: params[:input_name], 
+    price: params[:input_price], 
+    image_url: params[:input_image_url], 
+    description: params[:input_description]
+  )
+    # product.save # not needed when update method is used.
+    render json: params.as_json
+  end
 end
