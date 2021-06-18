9542e0d7ffba6799bfb6267b1c77f494c51a45ff
mike <mmorton7295@gmail.com>
2021-06-17 19:23:42 -0500

    removed non-REST routes and controller methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,3 @@
 class ProductsController < ApplicationController
-  def give_one_product
-    product = Product.all[rand(0..(Product.all.length-1))]
-    render json: product.as_json
-  end
-  
-  def give_all_products
-    render json: Product.all.as_json
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/random_product" => "products#give_one_product"
-  get "/all_products" => "products#give_all_products"
 end
