b2e7a7cf087980ce62b2fe0530faf085558168d2
mike <mmorton7295@gmail.com>
2021-06-23 21:25:35 -0500

    adds supplier model, adds supplier_id to product, updates seeds file for new schema

---
+++ db/migrate/20210624012513_create_supplier.rb
@@ -0,0 +1,11 @@
+class CreateSupplier < ActiveRecord::Migration[6.1]
+  def change
+    create_table :suppliers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_233739) do
+ActiveRecord::Schema.define(version: 2021_06_24_013217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2021_06_22_233739) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "amount_in_inventory"
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
