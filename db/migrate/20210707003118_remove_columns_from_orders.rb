ab3cc72d0fc59fa8987d5a40b4f81f4aff565f46
Mike Morton <mmorton7295@gmail.com>
2021-07-06 19:37:22 -0500

    updates orders table to new format, creates carted_products table

---
+++ db/migrate/20210707003118_remove_columns_from_orders.rb
@@ -0,0 +1,6 @@
+class RemoveColumnsFromOrders < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :orders, :product_id, :integer
+    remove_column :orders, :quantity, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_233007) do
+ActiveRecord::Schema.define(version: 2021_07_07_003434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carted_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "quantity"
+    t.integer "user_id"
+    t.integer "order_id"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -37,8 +47,6 @@ ActiveRecord::Schema.define(version: 2021_06_30_233007) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "product_id"
-    t.integer "quantity"
     t.decimal "subtotal", precision: 9, scale: 2
     t.decimal "tax", precision: 9, scale: 2
     t.decimal "total", precision: 9, scale: 2
